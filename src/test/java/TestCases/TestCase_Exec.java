885c04e4ce62bd69207e1e46d56e40e512bff99a
emilioorozco1 <eorozco2@ucol.mx>
2022-04-07 13:33:58 -0500

    Practice exercise completed

---
+++ src/test/java/TestCases/TestCase_Exec.java
@@ -0,0 +1,118 @@
+package TestCases;
+
+import org.junit.*;
+import org.junit.rules.TestName;
+
+import com.aventstack.extentreports.Status;
+import POM.AtLoginPage;
+import selenium.ClsBrowser;
+import selenium.ClsReport;
+
+public class TestCase_Exec extends ClsBrowser
+{
+    @Rule //Missing import
+    public TestName TC_Name = new TestName();
+    public String URL;
+
+    @BeforeClass
+    public static void beforeClass()
+    {
+        ClsReport.fnSetupReport();
+    }
+
+    @Before
+    public void setup()
+    {
+        ClsBrowser.BrowserName = "Chrome";
+        OpenBrowser();
+    }
+
+
+    @Test
+    public void FirstTC()
+    {
+        try
+        {
+            ClsReport.objTest = ClsReport.objExtent.createTest("First Test");
+            URL = "https://positionsapp-uat.azurewebsites.net/#";
+            NavigateToUrl(URL);
+            WaitForLoad();
+            ClsReport.fnLog(Status.PASS, "First Log Comment.", false);
+            AtLoginPage objLogin = new AtLoginPage();
+            objLogin.enterCredential();
+            objLogin.startSession();
+            objLogin.keepSessionDialog();
+            objLogin.verifyActiveSession();
+            ClsReport.fnLog(Status.PASS, "Second Log Comment.", true);
+        }
+        catch(Exception e)
+        {
+            ClsReport.fnLog(Status.FAIL, "The: " + TC_Name.getMethodName() + " was not executed successfully. \n Exception: " + e.getMessage() , false);
+        }
+    }
+
+
+    @Test
+    public void SecondTC()
+    {
+        try
+        {
+            ClsReport.objTest = ClsReport.objExtent.createTest("Second Test");
+            URL = "https://positionsapp-uat.azurewebsites.net/#"; //Changed url
+            NavigateToUrl(URL);
+            WaitForLoad();
+            ClsReport.fnLog(Status.PASS, "First Log Comment.", false);
+            AtLoginPage objLogin = new AtLoginPage();
+            objLogin.enterCredential();
+            objLogin.startSession();
+            objLogin.keepSessionDialog();
+            objLogin.verifyActiveSession();
+            ClsReport.fnLog(Status.PASS, "Second Log Comment.", true);
+        }
+        catch (Exception e)
+        {
+            ClsReport.fnLog(Status.FAIL, "The: " + TC_Name.getMethodName() + " was not executed successfully. \n Exception: " + e.getMessage() , false);
+        }
+    }
+
+
+    @Test
+    public void NewTest()
+    {
+        try
+        {
+            ClsReport.objTest = ClsReport.objExtent.createTest("Second Test");
+            URL = "https://positionsapp-uat.azurewebsites.net/#"; //Changed url
+            NavigateToUrl(URL);
+            WaitForLoad();
+            ClsReport.fnLog(Status.PASS, "First Log Comment.", false);
+            AtLoginPage objLogin = new AtLoginPage();
+            objLogin.enterCredential();
+            objLogin.startSession();
+            objLogin.keepSessionDialog();
+            objLogin.verifyActiveSession();
+            ClsReport.fnLog(Status.PASS, "Second Log Comment.", true);
+        }
+        catch (Exception e)
+        {
+            ClsReport.fnLog(Status.FAIL, "The: " + TC_Name.getMethodName() + " was not executed successfully. \n Exception: " + e.getMessage() , false);
+        }
+    }
+
+
+
+    @After
+    public void tearDown()
+    {
+        CloseBrowser();
+        ClsReport.fnCloseReport();
+    }
+
+    //@After
+    //public void tearDown()
+    //{
+       // CloseBrowser();
+     //   ClsReport.fnCloseReport();        Repeated code
+   // }
+
+}
